2865f83b3f5b41d3d1b762422bd48780808f6e9f
iamilyaalex <blagas77@me.com>
2024-04-09 00:30:04 +0300

    added new file

---
+++ test.py
@@ -0,0 +1,15 @@
+# import os
+
+
+# with open("text.txt", "r", encoding="utf-8") as file:
+# 	print(file.read())
+
+
+# with open("text.txt", "w", encoding="utf-8") as file:
+# 	file.write("some str")
+
+# with open("text.txt", "a", encoding="utf-8") as file:
+# 	file.write("added str")
+
+
+# print(os.getcwd())
